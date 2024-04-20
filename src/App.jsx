b8c291d33fb5ca8d9f2cd070e321b4d27a777ff9
JAKUAN-AHMED <jakuanultimate777@gmail.com>
2024-04-20 16:01:59 +0600

    added custom navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import DaisyNav from "./components/DaIsyNav/DaisyNav";
+import NavBar from "./components/NavBar/NavBar";
+function App() {
+  return (
+    <>
+      <NavBar></NavBar>
+      <DaisyNav></DaisyNav>
+      <h1 className="text-7xl bg-rose-600">Vite+React</h1>
+    </>
+  );
+}
+
+export default App;
